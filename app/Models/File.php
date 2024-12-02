41d718ace35b7860729b8b33b404779600d888ba
Ricardo <rycardo.191@gmail.com>
2024-12-02 02:17:02 -0300

    FileModel ajustes

--- app/Models/File.php
+++ app/Models/File.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'path', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
